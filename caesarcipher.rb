9c3380c3fb91bf4c2a19e4ba41071761b1c66c68
Joel Baxter <joel.eliot.baxter@gmail.com>
2018-09-11 17:29:49 +0100

    Update caesarcipher.rb

--- caesarcipher.rb
+++ caesarcipher.rb
@@ -1,51 +1,52 @@
 #This is a basic caesar cipher. Only letters [A-Za-z] are affected. I added a decrypt function, but it requires that the user knows the value of the shift to perform the decryption.
 
 class CaesarCipher
-  @@uppercase = *("A".."Z")
-  @@lowercase = *("a".."z")
-
+  
   attr :string, :shift
 
   def initialize(string, shift=1)
     @string = string
     @shift = shift
+    @uppercase = *("A".."Z")
+    @lowercase = *("A".."Z")
   end
 
   def encrypt
-    encryptors
-    string.chars.map { |char|
-      if @@uppercase.include? char
+    load_encryptors
+    string.chars.map do |char|
+      if @uppercase.include? char
         @uppercase_encryptor.fetch(char)
-      elsif @@lowercase.include? char
+      elsif @lowercase.include? char
         @lowercase_encryptor.fetch(char)
       else char
-      end }.join
+      end
+    end
+     .join
   end
 
   def decrypt
-    decryptors
-    string.chars.map { |char|
-      if @@uppercase.include? char
+    load_decryptors
+    string.chars.map do |char|
+      if @uppercase.include? char
         @uppercase_decryptor.fetch(char)
-      elsif @@lowercase.include? char
+      elsif @lowercase.include? char
         @lowercase_decryptor.fetch(char)
       else char
-      end }.join
+      end
+    end
+    .join
   end
 
-  private
-
-  def encryptors
+  def load_encryptors
     @uppercase_encryptor = Hash[@@uppercase.zip(@@uppercase.rotate(shift))]
     @lowercase_encryptor = Hash[@@lowercase.zip(@@lowercase.rotate(shift))]
   end
 
-  def decryptors
+  def load_decryptors
     @uppercase_decryptor = Hash[@@uppercase.zip(@@uppercase.rotate(-shift))]
     @lowercase_decryptor = Hash[@@lowercase.zip(@@lowercase.rotate(-shift))]
   end
 
+  private_method_name :load_encryptors, :load_decryptors
+  
 end
-
-cipher1 = CaesarCipher.new("Pmttw! Ug vium qa Rwmt.", 8)
-p cipher1.decrypt
